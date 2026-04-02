8552e686cfb403f1b0517ccf9e9b1073816f22b5
unknown <alch3mist94@protonmail.com>
2026-04-02 18:09:26 +0200

    improved run sh

--- AMD_LLM_vLLM_Benchmark/run_broad_bench_docker.sh
+++ AMD_LLM_vLLM_Benchmark/run_broad_bench_docker.sh
@@ -4,13 +4,14 @@ SERVICE_NAME="vbench" # The name defined in your docker-compose.yml
 
 # Print Start Message
 echo "[$(date +'%T')] vbench: Starting Docker Benchmarking Pipeline..."
+start_time=$(date +%s)
 
 # Ensure results directory exists
 mkdir -p ./results
 touch ./results/benchmark.log
 
 # Start the container
-docker compose run --rm vbench
+docker compose run -d --rm vbench
 
 echo "[vbench] Streaming logs from ./results/benchmark.log ..."
 tail -f ./results/benchmark.log & 
@@ -26,8 +27,15 @@ done
 # Kill the tail process
 kill $TAIL_PID
 
+end_time=$(date +%s)
+total_time_s=$((end_time - start_time))
+time_h=$((total_time_s / 3600))
+time_m=$(((total_time_s / 60) % 60))
+time_s=$((total_time_s % 60))
+
 # Print Stop Message
 echo "------------------------------------------"
 echo "[$(date +'%T')] STOPPED: Container has terminated."
 echo "Final results available in ./results/"
+echo "Total runtime: $time_h h  $time_m m  $time_s s"
 echo "------------------------------------------"
