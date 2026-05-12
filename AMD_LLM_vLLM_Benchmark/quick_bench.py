aca18aa1e836ebf9e839adc32aa39912293bc7c4
unknown <alch3mist94@protonmail.com>
2026-05-12 10:54:41 +0200

    bug fix in quick bench

--- AMD_LLM_vLLM_Benchmark/quick_bench.py
+++ AMD_LLM_vLLM_Benchmark/quick_bench.py
@@ -14,37 +14,43 @@
 
 import argparse
 from vbench import BaseBench, singleBenchmark, show
+from traceback import print_exc
 
 def main ():
 
-    # Load Parser
-    parser = argparse.ArgumentParser(description="LLM Benchmark Script For vLLM")
-    parser.add_argument("--hf_token", type=str, required=True, help="Hugging Face token")
-    parser.add_argument("--model", type=str, required=True, help="Model huggingface path, e.g., inceptionai/jais-13b-chat")
-    parser.add_argument("--max_model_len", type=int, default=-1, help="Max model len, default -1 for auto detect from model config. Should be larger than max_input_len + max_output_len")
-    parser.add_argument("--num_iterations", type=int, default=10, help="Total number of batched propagations to run per configuration.")
-    parser.add_argument("--dataset", type=int, default=10, help="random or small-mixed")
-    parser.add_argument("--warmup_runs", type=int, default=3, help="How many warmup iterations.")
-    parser.add_argument("--max_output_len", type=int, default=256, help="Max tokens to generate per request.")
-    parser.add_argument("--max_input_len", type=int, default=256, help="Max tokens to input per request. Note: this parameter only works with random dataset.")
-    parser.add_argument("--batch_size", type=int, default=1, help="The number of prompts processed concurrently.")
-    parser.add_argument("--temperature", type=float, default=0, help="The sampling temperature, default=0")
-    parser.add_argument("--top_p", type=float, default=1.0, help="Controls the cumulative probability of the top tokens to consider. Must be in (0, 1]. Set to 1 to consider all tokens.")
-    parser.add_argument("--device_type", type=str, default="GPU", help="Select device type GPU or CPU. Default: GPU")
-    args = parser.parse_args()
+    try:
 
-    # Perform benchmark
-    bench = BaseBench(args.model, args.hf_token, "n/a", args.max_model_len)
-    results = singleBenchmark(bench, 
-                              args.batch_size,
-                              args.num_iterations,
-                              args.dataset, 
-                              args.max_input_len, 
-                              args.max_output_len, 
-                              args.warmup_runs,
-                              args.temperature,
-                              args.top_p,
-                              args.device_type)
+        # Load Parser
+        parser = argparse.ArgumentParser(description="LLM Benchmark Script For vLLM")
+        parser.add_argument("--hf_token", type=str, required=True, help="Hugging Face token")
+        parser.add_argument("--model", type=str, required=True, help="Model huggingface path, e.g., inceptionai/jais-13b-chat")
+        parser.add_argument("--max_model_len", type=int, default=-1, help="Max model len, default -1 for auto detect from model config. Should be larger than max_input_len + max_output_len")
+        parser.add_argument("--num_iterations", type=int, default=10, help="Total number of batched propagations to run per configuration.")
+        parser.add_argument("--dataset", type=int, default=10, help="random or small-mixed")
+        parser.add_argument("--warmup_runs", type=int, default=3, help="How many warmup iterations.")
+        parser.add_argument("--max_output_len", type=int, default=256, help="Max tokens to generate per request.")
+        parser.add_argument("--max_input_len", type=int, default=256, help="Max tokens to input per request. Note: this parameter only works with random dataset.")
+        parser.add_argument("--batch_size", type=int, default=1, help="The number of prompts processed concurrently.")
+        parser.add_argument("--temperature", type=float, default=0, help="The sampling temperature, default=0")
+        parser.add_argument("--top_p", type=float, default=1.0, help="Controls the cumulative probability of the top tokens to consider. Must be in (0, 1]. Set to 1 to consider all tokens.")
+        parser.add_argument("--device_type", type=str, default="GPU", help="Select device type GPU or CPU. Default: GPU")
+        args = parser.parse_args()
+
+        # Perform benchmark
+        bench = BaseBench(args.model, args.hf_token, "n/a", args.max_model_len)
+        results = singleBenchmark(bench, 
+                                args.batch_size,
+                                args.num_iterations,
+                                args.dataset, 
+                                args.max_input_len, 
+                                args.max_output_len, 
+                                args.warmup_runs,
+                                args.temperature,
+                                args.top_p,
+                                args.device_type)
+        
+        # Output
+        show(results)
     
-    # Output
-    show(results)
+    except Exception:
+        print_exc()
